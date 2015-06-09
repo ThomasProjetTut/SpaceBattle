770580ad7b97912d80856b6fb5835c7f43ff8a3c
Johan <johanvdp12@gmail.com>
2015-06-09 20:16:49 +0200

    grg

--- src/Control/ControlChat.java
+++ src/Control/ControlChat.java
@@ -24,20 +24,22 @@ public class ControlChat extends KeyAdapter {
     public void keyPressed(KeyEvent e) {
 
         if (e.getKeyCode() == 10) {
+        	
         	if (vueJeu.getChatNomJoueur().getText().isEmpty())
         		VueJeu.getChatTexte().append("Vous devez mettre un nom avant de parler.\n");
         	else if (!vueJeu.getChatLigne().getText().isEmpty() && e.getSource() == vueJeu.getChatLigne()) {
             	Model.getJoueur1().setNomJoueur(vueJeu.getChatNomJoueur().getText());
             	
-            	if (VueConnexion.isHost()) {
-            		ServeurTCP.getOutPut().println("C"+Model.getJoueur1().getNomJoueur() + " : " + vueJeu.getChatLigne().getText() + "\n");
-            		ServeurTCP.getOutPut().println("N"+Model.getJoueur1().getNomJoueur());
+            	if (Model.jeuIsEnMulti()) {
+	            	if (VueConnexion.isHost()) {
+	            		ServeurTCP.getOutPut().println("C"+Model.getJoueur1().getNomJoueur() + " : " + vueJeu.getChatLigne().getText() + "\n");
+	            		ServeurTCP.getOutPut().println("N"+Model.getJoueur1().getNomJoueur());
+	            	}
+	            	else {
+	            		ClientTCP.getOutPut().println("C"+Model.getJoueur1().getNomJoueur() + " : " + vueJeu.getChatLigne().getText() + "\n");
+	            		ClientTCP.getOutPut().println("N"+Model.getJoueur1().getNomJoueur());
+	            	}
             	}
-            	else {
-            		ClientTCP.getOutPut().println("C"+Model.getJoueur1().getNomJoueur() + " : " + vueJeu.getChatLigne().getText() + "\n");
-            		ClientTCP.getOutPut().println("N"+Model.getJoueur1().getNomJoueur());
-            	}
-            	
             	VueJeu.getChatTexte().append(Model.getJoueur1().getNomJoueur() + " : " + vueJeu.getChatLigne().getText() + "\n");
             	
             	vueJeu.getChatLigne().setText("");
